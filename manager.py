bd6b803534f408449a6e2ae917b1fcd2f985aeb1
a603558762 <603558762@qq.com>
2018-06-19 11:13:54 +0800

    集成数据库迁移和flask_script

--- manager.py
+++ manager.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
+from flask_session import Session
 
 app = Flask(__name__)
+manager=Manager(app)
 
+
+# 配置文件
 class Config(object):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='mysql://root:hh123456@localhost3306/information'
@@ -12,9 +18,23 @@ class Config(object):
     # redis配置
     REDIS_HOST='127.0.0.1'
     REDIS_PORT=6379
+    # Session 配置
+    SESSION_TYPE='redis'
+    # 开启签名
+    SESSION_USE_SIGNER=True
+    # 设置redis初始对象
+    SESSION_REDIS=StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+
+
+    # 设置过期时间
+    SESSION_PERMANENT=False
 
 app.config.from_object(Config)
 db=SQLAlchemy(app)
+
+Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
 
 # 配置redis
 redis_store=StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
@@ -27,4 +47,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
