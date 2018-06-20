87b44bbb0d7729de287af7050a97ddc1c1ed77a2
a603558762 <603558762@qq.com>
2018-06-20 15:21:06 +0800

    导入短信验证需要的文件

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import session, render_template
+from flask import session, render_template, current_app
 
 # from info import redis_store
 from . import Index_blu
@@ -15,9 +15,11 @@ def index():
     session['aaa']='wangwnagban'
 
     # logging.debug('错误1111')
+    return render_template('news/index.html')
 
 
 
-
-
-    return render_template('index.html')
+@Index_blu.route('/favicon.ico')
+def favicon():
+    # 固定写法,每个网站都是固定的写法有这个favicon.ico
+    return current_app.send_static_file('news/favicon.ico')
--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -153,6 +153,8 @@ var imageCodeId = ""
 // TODO 生成一个图片验证码的编号，并设置页面中图片验证码img标签的src属性
 function generateImageCode() {
 
+
+
 }
 
 // 发送短信验证码
